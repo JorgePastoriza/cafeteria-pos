03db5b0a0dca7b365ee71eddb0bfe30811ac091c
Jorge Pastoriza <jorge.pastoriza@gmail.com>
2026-03-07 21:30:33 -0300

    update role.js

--- backend/src/models/Role.js
+++ backend/src/models/Role.js
@@ -6,6 +6,6 @@ const Role = sequelize.define('Role', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING(50), allowNull: false, unique: true },
   description: { type: DataTypes.STRING(255) }
-}, { tableName: 'roles' });
+}, { tableName: 'roles', timestamps: false });
 
 module.exports = Role;
